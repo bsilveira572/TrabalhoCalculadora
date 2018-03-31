248531bd38efe00bbd6473ce7a860bdbd9a90392
Bruno da Silveira Ramos <37782051+bsilveira572@users.noreply.github.com>
2018-03-30 22:13:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <body>
         <h1>Calculadora</h1>
         <form method="post" action="index.php">
-            <input type="text" step="0.001" name="valor1" size='20'><br><br>
-            <input type="text" step="0.001" name="valor2" size='20'><br><br>
+            <input type="text" step="0.001" name="valor1" size='23'><br><br>
+            <input type="text" step="0.001" name="valor2" size='23'><br><br>
             <select name="operacao">
                 <option value='somar'>Soma</option>
                 <option value='subtrair'>Subtração</option>
                 <option value='multiplicar'>Multiplicação</option>
                 <option value='dividir'>Divisão</option>
-            </select><br><br>
+            </select>
             <input type="submit" value="Calcular">
         </form>
         <?php
